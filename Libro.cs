0435229abe8608c86800853dbe88061d4643a521
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-27 13:29:18 +0200

    ok

---
+++ Libro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Libro
+{
+
+    private int isbn;
+    private string titulo;
+    private string autor;
+    private int numPaginas;
+
+    public int Isbn { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public int NumPaginas { get; set; }
+
+    public int getNumPaginas()
+    {
+        return this.numPaginas;
+    }
+
+
+    public Libro() { }
+
+    public Libro(int _isbn, string _titulo, string _autor, int _numPaginas)
+    {
+        this.isbn = _isbn;
+        this.titulo = _titulo;
+        this.autor = _autor;
+        this.numPaginas = _numPaginas;
+    }
+
+    public override string ToString()
+    {
+        return "El libro " + titulo + " con ISBN " + isbn + " creado por el autor " + autor + " tiene " + numPaginas + " páginas";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T11_Ejerciocio6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Libro quijote = new Libro(9568686, "El Quijote", "Cervantes", 2000);
+            Console.WriteLine(quijote.ToString());
+            Libro diaPerfecto = new Libro(90989786, "Un día perfecto", "Ira Levin", 300);
+            Console.WriteLine(diaPerfecto.ToString());
+
+            if (quijote.getNumPaginas() > diaPerfecto.getNumPaginas())
+            {
+                Console.WriteLine("El quijote tiene más páginas");
+            }
+            else
+            {
+                Console.WriteLine("Un día perfecto tiene más páginas");
+            }
+        }
+    }
+}
